855fd1da32568ca79f0bc33d8a684d07b7f0fbc8
bodenko victor <bodenko.vv@gmail.com>
2018-10-30 16:14:25 +0200

    add controller, route homework with block and template

---
+++ app/code/BodenkoVV/CustomModule/Controller/Index/Index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vector
+ * Date: 30.10.18
+ * Time: 15:42
+ */
+namespace BodenkoVV\CustomModule\Controller\Index;
+
+/*
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->_pageFactory->create();
+    }
+}
+*/
+
+
+use Magento\Framework\Controller\ResultFactory;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $geethubText = "Home-work 3";
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getLayout()->getBlock('custom.lesson.page.result')->setGeethubText($geethubText);
+        return $resultPage;
+    }
+}
---
+++ app/code/BodenkoVV/CustomModule/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vector
+ * Date: 26.10.18
+ * Time: 17:44
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'BodenkoVV_CustomModule',
+        __DIR__
+    );
