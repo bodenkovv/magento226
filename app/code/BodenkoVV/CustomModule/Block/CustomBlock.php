665595ca9a162db4538b962e884fadeb1e477cb7
bodenko victor <bodenko.vv@gmail.com>
2018-10-31 19:59:51 +0200

    add function in block and use function

--- app/code/BodenkoVV/CustomModule/Block/CustomBlock.php
+++ app/code/BodenkoVV/CustomModule/Block/CustomBlock.php
@@ -1,14 +1,17 @@
 <?php
+namespace BodenkoVV\CustomModule\Block;
+
 /**
  * Created by PhpStorm.
  * User: vector
  * Date: 30.10.18
  * Time: 19:27
  */
-namespace BodenkoVV\CustomModule\Block;
+
+use Magento\Framework\Controller\ResultFactory;
 class CustomBlock extends \Magento\Framework\View\Element\Template
 {
-//    const HOMEWORK_TEMPLATE = "BodenkoVV_CustomModule::customblock.phtml";
+    //   const HOMEWORK_TEMPLATE = "BodenkoVV_CustomModule::customblock.phtml";
     //   const HOMEWORK_TEMPLATE = "BodenkoVV_CustomModule::myhomework.phtml";
 
     /*
@@ -20,5 +23,33 @@ class CustomBlock extends \Magento\Framework\View\Element\Template
         return $this;
     }
 */
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+
+    public $myurl="99999";
+
+    public function myurltext($myurl)
+    {
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+
+        //$resultPage = $this->resultFactory->create(ResultFactory::TYPE_LAYOUT);
+        //$resultPage ->$block->getUrl('homework/JsonResponse');
+        //$block->getUrl('homework/JsonResponse');
+
+        //$myurl=$resultPage ->$block->getUrl('homework/JsonResponse');
+        //$myurl=getUrl($myurl);
+
+        //$resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        //$resultPage->getLayout()->getBlock('custom.lesson.page.myurltext')->setMyUrlText($myurl);
+        //$resultPage->getLayout()->getBlock('custom.lesson.page.myurltext')->getUrl($myurl);
+        //$myurl=getUrl('homework/JsonResponse');
+        //return $resultPage;
+        //return $myurl;
+        return $this->getUrl($myurl);
+
+    }
+
 }
 
---
+++ app/code/BodenkoVV/CustomModule/Controller/Block/Index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vector
+ * Date: 30.10.18
+ * Time: 15:42
+ */
+namespace BodenkoVV\CustomModule\Controller\Block;
+
+/*
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->_pageFactory->create();
+    }
+}
+*/
+
+use Magento\Framework\Controller\ResultFactory;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+
+    public function execute()
+    {
+        /**
+         * $this->_view->loadLayout();
+         * $this->_view->renderLayout();
+         */
+        $homeworkText = "_Home-work 3 - Block _";
+
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        //$resultPage->getLayout()->getBlock('custom.lesson.page.result')->setGeethubText($geethubText);
+        $resultPage->getLayout()->getBlock('custom.lesson.page.myurltext')->setHomeworkText($homeworkText);
+
+        return $resultPage;
+    }
+}
