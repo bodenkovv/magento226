ee96937217e9b260ffc59ef3fb046c5ba3eec709
bodenko victor <bodenko.vv@gmail.com>
2018-12-10 11:58:43 +0200

    del comment in block

--- app/code/BodenkoVV/Homework5/Block/CustomBlock.php
+++ app/code/BodenkoVV/Homework5/Block/CustomBlock.php
@@ -4,8 +4,6 @@ namespace BodenkoVV\Homework5\Block;
 class CustomBlock extends \Magento\Customer\Block\Form\Register
 {
     public $CountryHtmlSelect, $ShowAddressFields = true;
-//    public $name, $id, $title, $defValue;
-//    public $_company,$_telephone,$_gender,$_fax,$_streetValidationClass;
 
     public function getShowAddressFields()
     {
