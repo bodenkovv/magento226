9ad282ac82bdda19cfb37c26dea87db7fda9fa24
bodenko victor <bodenko.vv@gmail.com>
2018-12-11 18:46:49 +0200

    homework7: fix

--- app/design/frontend/vector/theme2/Magento_Catalog/web/js/m2kocomponent.js
+++ app/design/frontend/vector/theme2/Magento_Catalog/web/js/m2kocomponent.js
@@ -1,17 +1,30 @@
-define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
+define([
+    'jquery',
+    'uiComponent',
+    'ko'],
+    function ($, Component, ko) {
     'use strict';
+
     var self;
+
     return Component.extend({
         myTimer: ko.observable(0),
-        red: ko.observable(0),
-        blue: ko.observable(0),
-        green: ko.observable(0),
-        redPre: ko.observable(0),
-        bluePre: ko.observable(0),
-        greenPre: ko.observable(0),
+        color: {
+            red: ko.observable(0),
+            blue: ko.observable(0),
+            green: ko.observable(0)
+        },
+        colorPreview: {
+            red: ko.observable(0),
+            blue: ko.observable(0),
+            green: ko.observable(0)
+        },
+        randomTop: ko.observable(0),
+        randomLeft: ko.observable(0),
         randomTopp: ko.observable(0),
         randomLeftt: ko.observable(0),
         randomPos: ko.observable(0),
+        randomColour: ko.observable(0),
         randomColourPreview: ko.observable(0),
         initialize: function () {
             self = this;
@@ -19,61 +32,58 @@ define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
             //call the incrementTime function to run on intialize
             this.incrementTime();
             this.subscribeToTime();
-            this.randomPos="relative";
+            this.randomPos = 'relative';
 
-            this.randomColour = ko.computed(function() {
-                return 'rgb(' + this.red() + ', ' + this.blue() + ', ' + this.green() + ')';
+            this.randomColour = ko.computed(function () {
+                return 'rgb(' + this.color.red() + ', ' + this.color.blue() + ', ' + this.color.green() + ')';
             }, this);
 
-            this.randomColourPreview = ko.computed(function() {
-                return 'rgb(' + this.redPre() + ', ' + this.bluePre() + ', ' + this.greenPre() + ')';
+            this.randomColourPreview = ko.computed(function () {
+                return 'rgb(' + this.colorPreview.red() + ', ' + this.colorPreview.blue() + ', ' + this.colorPreview.green() + ')';
             }, this);
 
             //return the random Top value
-            this.randomTop = ko.computed(function() {
+            this.randomTop = ko.computed(function () {
                 return '' + this.randomTopp() + 'px';
             }, this);
             //return the random Top value
-            this.randomLeft = ko.computed(function() {
+            this.randomLeft = ko.computed(function () {
                 return '' + this.randomLeftt() + 'px';
             }, this);
 
         },
         //increment myTimer every second
-        incrementTime: function() {
+        incrementTime: function () {
+
             var t = 0;
-            setInterval(function() {
+
+            setInterval(function () {
                 t++;
                 self.myTimer(t);
             }, 1000);
         },
-        subscribeToTime: function() {
-            this.myTimer.subscribe(function(newValue) {
+        subscribeToTime: function () {
+            this.myTimer.subscribe(function (newValue) {
                 //console.log(newValue);
                 self.updateTimerTextColour();
                 self.updateTimerPosition();
             });
         },
-        randomNumber: function() {
+        randomNumber: function () {
             return Math.floor((Math.random() * 255) + 1);
         },
-        updateTimerTextColour: function() {
-            //define RGB values
-            /*notice we now no longer have to set and return the RBG style code here
-             we simply update the red/blue/green observables and the computed observable
-             returns the style element to the template */
+        updateTimerTextColour: function () {
+            this.colorPreview.red(this.color.red());
+            this.colorPreview.blue(this.color.blue());
+            this.colorPreview.green(this.color.green());
 
-            this.redPre(this.red());
-            this.bluePre(this.blue());
-            this.greenPre(this.green());
-
-            this.red(self.randomNumber());
-            this.blue(self.randomNumber());
-            this.green(self.randomNumber());
+            this.color.red(self.randomNumber());
+            this.color.blue(self.randomNumber());
+            this.color.green(self.randomNumber());
         },
-        updateTimerPosition: function() {
-            this.randomTopp(self.randomNumber()-self.randomNumber());
-            this.randomLeftt(self.randomNumber()-self.randomNumber());
+        updateTimerPosition: function () {
+            this.randomTopp(self.randomNumber());
+            this.randomLeftt(self.randomNumber());
         }
     });
 });
